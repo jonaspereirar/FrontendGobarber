9850a28871d96c61b40c2befc1ab3ec5c9230b25
jonaspereirar <jonasprodrigues@gmail.com>
2020-01-23 21:48:34 +0000

    feat add.:  Configured Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router } from 'react-router-dom'
+
+import Routes from './routes';
+import history from './services/history'
 
 function App() {
-  return <div className="App" />
-
+  return <Router history={history}><Routes /></Router>;
 }
 
 export default App;
